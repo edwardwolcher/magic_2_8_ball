f54165fffb1893cc1bbe77830ca1a4f1ea88fdb4
Edward Wolcher <65256014+edwardwolcher@users.noreply.github.com>
2021-07-14 23:41:07 -0700

    added p5js demo

--- build_text.py
+++ build_text.py
@@ -1,10 +1,14 @@
 # This quick, sloopy python script reads the 'nouns.txt' and 'verbs.txt' files and converts them
 # into a single 'Text.h' file in the magicBall.ino directory with proper c++ formatting that
 # allows them to be read into the program memory of the arduino. This script should be run whenever
-# a change is made to the text files.
+# a change is made to the text files. It also populates "text.js" in the demo folder to let the p5.js demo
+# sketch work.
 
 nouns = open(r"nouns.txt", "r")
 text = open("magicBall/Text.h", "w+")
+demoText = open("demo/text.js", "w+")
+demoText.write("const nouns = [")
+
 nounsPointer = "const char *const noun_table[] PROGMEM = {"
 for idx, noun in enumerate(nouns, 0):
     formattedNoun = 'const char n{idx}[] PROGMEM = "{noun}";\n'.format(
@@ -14,8 +18,9 @@ for idx, noun in enumerate(nouns, 0):
     if idx == 127:
         comma = ''
     nounsPointer += 'n{idx}{comma}'.format(idx=idx, comma=comma)
-
+    demoText.write('"{noun}"{comma}'.format(noun=noun.strip(), comma=comma))
 nounsPointer += '};'
+demoText.write('];\nconst verbs = [')
 text.write(nounsPointer)
 nouns.close()
 verbs = open(r"verbs.txt", "r")
@@ -28,6 +33,9 @@ for idx, verb in enumerate(verbs, 0):
     if idx == 127:
         comma = ''
     verbsPointer += 'v{idx}{comma}'.format(idx=idx, comma=comma)
+    demoText.write('"{verb}"{comma}'.format(verb=verb.strip(), comma=comma))
 verbsPointer += '};\n'
 text.write(verbsPointer)
+demoText.write('];')
+demoText.close()
 verbs.close()
---
+++ demo/script.js
@@ -0,0 +1,41 @@
+let noun, verb
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(240)
+  shakeBall()
+}
+
+function drawBall() {
+  push()
+  rectMode(CENTER)
+  ellipseMode(CENTER)
+  translate(width / 2, height / 2)
+  noStroke()
+  // draw Circle
+  const diameter = min(width, height) * 0.8
+  fill(10)
+  circle(0, 0, diameter)
+  // draw LCD
+  const rectWidth = min(width, height) * 0.4
+  const rectHeight = rectWidth * 0.5
+  fill(20, 20, 240)
+  rect(0, 0, rectWidth, rectHeight)
+  // draw Text
+  fill(240)
+  textFont('Monospace')
+  textSize(map(textWidth('abcdefghijklmnop'), 0, 80, 0, rectWidth / 16))
+  text(noun, -rectWidth / 2 * 0.9, -rectHeight * 0.1)
+  text(verb, -rectWidth / 2 * 0.9, rectHeight * 0.2)
+  pop()
+}
+
+function shakeBall() {
+  noun = random(nouns)
+  verb = random(verbs)
+  drawBall()
+}
+
+function keyPressed() {
+  shakeBall()
+}
